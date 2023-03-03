826c2b071c70a349efe0ee216e5f55d75e877094
Soyeon Lee <dingdingcrong@naver.com>
2023-03-03 13:09:39 +0900

    [Gold IV] Title: 연구소, Time: 2948 ms, Memory: 131736 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -0,0 +1,60 @@
+from collections import deque
+from copy import deepcopy
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    answer = 0
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+
+    def count(nboard):
+        result = 0
+        for i in range(N):
+            for j in range(M):
+                if nboard[i][j] == 0:
+                    result += 1
+        return result
+
+    def BFS(i, j, nboard):
+        queue = deque()
+        queue.append((i, j))
+        while queue:
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx < N and 0 <= ny < M and nboard[nx][ny] == 0:
+                    nboard[nx][ny] = 2
+                    queue.append((nx, ny))
+
+    def DFS(L):
+        nonlocal answer
+        if L == 3:
+            nboard = deepcopy(board)
+            visited = [[False] * M for _ in range(N)]
+            for i in range(N):
+                for j in range(M):
+                    if board[i][j] == 2 and not visited[i][j]:
+                        visited[i][j] = True
+                        BFS(i, j, nboard)
+            answer = max(answer, count(nboard))
+        else:
+            for i in range(N):
+                for j in range(M):
+                    if board[i][j] == 0:
+                        board[i][j] = 1
+                        DFS(L + 1)
+                        board[i][j] = 0
+
+    DFS(0)
+    return answer
+
+
+N, M = map(int, input().split())
+board = []
+for _ in range(N):
+    board.append(list(map(int, input().split())))
+print(solution())
