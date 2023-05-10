0c76af02064d37b1da5ecaa8a5028ed3c7f2bca1
Soyeon Lee <dingdingcrong@naver.com>
2023-05-11 02:09:32 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 31764 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,5 +1,6 @@
 import sys
-from collections import deque
+
+sys.setrecursionlimit(10**6)
 
 input = sys.stdin.readline
 
@@ -7,46 +8,40 @@ dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 N = int(input())
-board = [list(input()) for _ in range(N)]
-visited1 = [[False] * N for _ in range(N)]
-visited2 = [[False] * N for _ in range(N)]
+board = [list(input().rstrip()) for _ in range(N)]
 
 
-def BFS(x, y, color_weakness):
-    queue = deque()
-    queue.append((x, y, color_weakness))
-    while queue:
-        x, y, color_weakness = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N:
-                if not color_weakness:
-                    if not visited1[nx][ny] and board[x][y] == board[nx][ny]:
-                        visited1[nx][ny] = True
-                        queue.append((nx, ny, color_weakness))
-                else:
-                    if not visited2[nx][ny]:
-                        if board[x][y] == "B":
-                            if board[x][y] == board[nx][ny]:
-                                visited2[nx][ny] = True
-                                queue.append((nx, ny, color_weakness))
-                        else:
-                            if board[nx][ny] != "B":
-                                visited2[nx][ny] = True
-                                queue.append((nx, ny, color_weakness))
+def DFS(x, y):
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < N and 0 <= ny < N:
+            if not visited[nx][ny] and board[x][y] == board[nx][ny]:
+                visited[nx][ny] = True
+                DFS(nx, ny)
 
 
-count1 = 0
-count2 = 0
+count = 0
+visited = [[False] * N for _ in range(N)]
 for i in range(N):
     for j in range(N):
-        if not visited1[i][j]:
-            visited1[i][j] = True
-            BFS(i, j, False)
-            count1 += 1
-        if not visited2[i][j]:
-            visited2[i][j] = True
-            BFS(i, j, True)
-            count2 += 1
-print(count1, count2)
+        if not visited[i][j]:
+            visited[i][j] = True
+            DFS(i, j)
+            count += 1
+print(count, end=" ")
+
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == "G":
+            board[i][j] = "R"
+
+count = 0
+visited = [[False] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if not visited[i][j]:
+            visited[i][j] = True
+            DFS(i, j)
+            count += 1
+print(count)
