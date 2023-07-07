2e246b4fde17c0317dac9c9d3436df30855dd60c
Soyeon Lee <dingdingcrong@naver.com>
2023-07-07 23:34:30 +0900

    [Silver III] Title: 1, Time: 156 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    try:
+        n = int(input())
+    except:
+        break
+    result = 1
+    count = 1
+    while True:
+        if result % n == 0:
+            print(count)
+            break
+        else:
+            result = result * 10 + 1
+            count += 1
