a4de460c281266715eac9d0c49195ed8042568ff
Soyeon Lee <dingdingcrong@naver.com>
2023-08-28 09:52:13 +0900

    [Gold IV] Title: 치즈, Time: 88 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -7,8 +7,7 @@ dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 def BFS():
-    count = 0
-    melt = [[False] * n for _ in range(m)]
+    melt = []
     queue = deque()
     visited = [[False] * n for _ in range(m)]
     queue.append((0, 0))
@@ -22,26 +21,21 @@ def BFS():
                 if graph[nx][ny] == 0:
                     queue.append((nx, ny))
                 else:
-                    melt[nx][ny] = True
-                    count += 1
+                    melt.append((nx, ny))
                 visited[nx][ny] = True
-    for i in range(m):
-        for j in range(n):
-            if melt[i][j]:
-                graph[i][j] = 0
-    return count
+    for i, j in melt:
+        graph[i][j] = 0
+    return len(melt)
 
 time = 0
 m, n = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(m)]
 cheeze = sum([sum(x) for x in graph])
 
-for i in range(m):
-    for j in range(n):
-        if graph[i][j] == 0:
-            time += 1
-            count = BFS()
-            cheeze -= count
-            if not cheeze:
-                print(time, count, sep='\n')
-                sys.exit(0)
+while True:
+    time += 1
+    count = BFS()
+    cheeze -= count
+    if not cheeze:
+        print(time, count, sep='\n')
+        break
